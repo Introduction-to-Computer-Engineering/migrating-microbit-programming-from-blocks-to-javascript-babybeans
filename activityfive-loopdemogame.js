53b667ad56efeabede05fe312b8d3004762847f7
Jessica Gardner <48143153+babybeans@users.noreply.github.com>
2019-09-27 16:43:47 -0600

    Update and rename loopdemogame.js to activityfive-loopdemogame.js

--- activityfive-loopdemogame.js
+++ activityfive-loopdemogame.js
@@ -1,27 +1,27 @@
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.B, function () { 
     for (let yindex2 = 4; yindex2 >= 0; yindex2--) {
-        for (let xindex2 = 4; xindex2 >= 0; xindex2 -= 2) {
-            led.plot(xindex2, yindex2)
+        for (let xindex2 = 4; xindex2 >= 0; xindex2 -= 2) { //plots light every other led
+            led.plot(xindex2, yindex2)//plots led
             basic.pause(1500)
-            led.unplot(xindex2, yindex2);
+            led.unplot(xindex2, yindex2);//unplots led
         }
     }
 })
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.A, function () { //plots light every other led
     for (let yindex = 0; yindex <= 4; yindex++) {
         for (let xindex = 0; xindex <= 4; xindex += 2) {
-            led.plot(xindex, yindex)
+            led.plot(xindex, yindex) //plots led
             basic.pause(1500)
             //led.unplot(xindex, yindex)
         }
     }
 })
 input.onGesture(Gesture.Shake, function () {
-    while (condition != 0) {
+    while (condition != 0) { //counts down from three
         basic.showString("" + condition + " ")
         condition += -1
     }
-    if (condition == 0) {
+    if (condition == 0) {//displays image after countdown
         basic.showLeds(`
             . # . # .
             . # . # .
@@ -32,14 +32,14 @@ input.onGesture(Gesture.Shake, function () {
     }
 })
 
-input.onButtonPressed(Button.AB, function () {
+input.onButtonPressed(Button.AB, function () {// plots led one  at a time
    for(let i=0; i <=4; i++){
        for(let j=0; j<=4; j++){
-           led.plot(j, i);
+           led.plot(j, i); //plots led
            basic.pause(1500);
-           led.unplot(j, i);
+           led.unplot(j, i); //unplots led
        }
  }
 })
-let condition = 0
-condition = 3
+let condition = 3
+

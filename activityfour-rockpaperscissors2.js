3540964d4ca475532a0f752751c7b285bdfc77ef
Jessica Gardner <48143153+babybeans@users.noreply.github.com>
2019-09-27 16:37:43 -0600

    Update and rename rockpaperscissors2.js to activityfour-rockpaperscissors2.js

--- activityfour-rockpaperscissors2.js
+++ activityfour-rockpaperscissors2.js
@@ -1,6 +1,6 @@
 input.onGesture(Gesture.Shake, function () {
     hand = Math.randomRange(0, 2)
-    if (hand == 0) {
+    if (hand == 0) { //displays  paper
         basic.showLeds(`
             # # # # #
             # . . . #
@@ -8,7 +8,7 @@ input.onGesture(Gesture.Shake, function () {
             # . . . #
             # # # # #
             `)
-    } else if (hand == 1) {
+    } else if (hand == 1) {//displays rock
         basic.showLeds(`
             . . . . .
             . # # # .
@@ -16,7 +16,7 @@ input.onGesture(Gesture.Shake, function () {
             . # # # .
             . . . . .
             `)
-    } else {
+    } else { //displays scissors
         basic.showLeds(`
             # # . . #
             # # . # .
@@ -26,17 +26,17 @@ input.onGesture(Gesture.Shake, function () {
             `)
     }
 })
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.A, function () { //adds to the score
     game.addScore(1)
     basic.pause(100)
     basic.showString("Wins: ")
     basic.showNumber(game.score())
-    if (game.score() == 5) {
+    if (game.score() == 5) { //checks to see if user wins
         basic.showString("You win!")
         control.reset();
     }
 })
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.B, function () { // subtracts from the score
     game.addScore(-1)
     basic.pause(100)
     basic.showString("Loss: ")

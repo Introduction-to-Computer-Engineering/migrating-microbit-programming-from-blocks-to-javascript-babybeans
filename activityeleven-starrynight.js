7303ef40b07c924d70a3f6fea8b054091d4da079
Jessica Gardner <48143153+babybeans@users.noreply.github.com>
2019-09-27 16:58:41 -0600

    Update and rename starrynight.js to activityeleven-starrynight.js

--- activityeleven-starrynight.js
+++ activityeleven-starrynight.js
@@ -1,12 +1,12 @@
 
 
 
-let arrayNums = [3, 5, 7, 1, 2];
+let arrayNums = [3, 5, 7, 1, 2];//creates array
 
 input.onButtonPressed(Button.A, function () {
     for (let i = 0; i < arrayNums.length; i++) {
         for (let j = 0; j < arrayNums[i] - 1; j++) {
-            led.plot(Math.randomRange(0, 5), Math.randomRange(0, 5))
+            led.plot(Math.randomRange(0, 5), Math.randomRange(0, 5))//displays a random amount of randomly plotted leds
         }
     }
 
@@ -15,14 +15,14 @@ input.onButtonPressed(Button.A, function () {
 
 })
 
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.B, function () {//displays elements in array
     for (let value of arrayNums) {
         basic.showNumber(value);
     }
     basic.clearScreen()
 })
 
-input.onButtonPressed(Button.AB, function () {
+input.onButtonPressed(Button.AB, function () {//displays elements + 1 
     for (let value of arrayNums) {
         value += 1;
         basic.showNumber(value)

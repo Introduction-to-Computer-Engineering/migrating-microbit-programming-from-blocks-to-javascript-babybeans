3e5d31495c973ca25fd8cb6249ff9fc3809f32fd
Jessica Gardner <48143153+babybeans@users.noreply.github.com>
2019-09-27 16:31:59 -0600

    Added comments and changed name .
    These changes make it easier to view.

--- activitythree-rockpaperscissors.js
+++ activitythree-rockpaperscissors.js
@@ -1,4 +1,4 @@
-let playerOne = 0
+let playerOne = 0 
 let playerTwo = 0
 let tie = 0
 let total = 0
@@ -8,18 +8,18 @@ let num = 0
 
 
 
-input.onButtonPressed(Button.A, function () {
-    playerOne += 1
-    total += 1
-    basic.showLeds(`
+input.onButtonPressed(Button.A, function () { // if player one wins
+    playerOne += 1  // adds to player one score
+    total += 1 // adds to  total
+    basic.showLeds(`  
     # . . . # 
     . # . # .
     . . # . .
     . # . # .
     # . . . #
     `)
-    basic.showNumber(playerOne)
-    if (playerOne == 5) {
+    basic.showNumber(playerOne) //displays score
+    if (playerOne == 5) { //checks if player one wins the game
         basic.showLeds(`
         # . . . #
         . # . # .
@@ -27,10 +27,10 @@ input.onButtonPressed(Button.A, function () {
         . # . # .
         # . . . #`)
         let percentAString = ""
-        let percentA = (playerOne / total) * 100;
+        let percentA = (playerOne / total) * 100; //displays percent of player one wins 
 
         percentAString = percentA.toString();
-        percentAString = percentAString.substr(0, percentAString.indexOf('.') + 2)
+        percentAString = percentAString.substr(0, percentAString.indexOf('.') + 2)//sets percent to the tenth place
         basic.showString(" wins with a" + percentAString + " % win rate")
         basic.clearScreen()
         playerOne = 0
@@ -38,9 +38,9 @@ input.onButtonPressed(Button.A, function () {
         tie = 0
     }
 })
-input.onButtonPressed(Button.B, function () {
-    playerTwo += 1
-    total += 1;
+input.onButtonPressed(Button.B, function () { //if  player two winss
+    playerTwo += 1 //adds to player two score
+    total += 1; //adds to total
     basic.showLeds(`
     . # # # . 
     # . . # # 
@@ -48,8 +48,8 @@ input.onButtonPressed(Button.B, function () {
     # # . . # 
     . # # # . 
     `)
-    basic.showNumber(playerTwo)
-    if (playerTwo == 5) {
+    basic.showNumber(playerTwo)//displays player two score
+    if (playerTwo == 5) { //checks if player two won the game
         basic.showLeds(`
         . # # # .
         # . . # #
@@ -59,7 +59,7 @@ input.onButtonPressed(Button.B, function () {
         `)
         let percentB = (playerTwo / total) * 100;
         let percentBString = percentB.toString();
-        percentBString = percentBString.substr(0, percentBString.indexOf('.') + 2)
+        percentBString = percentBString.substr(0, percentBString.indexOf('.') + 2) //rounds to the tenth place
         basic.showString(" wins with a " + percentBString + "% win rate")
         basic.clearScreen()
         playerOne = 0;
@@ -67,9 +67,9 @@ input.onButtonPressed(Button.B, function () {
         tie = 0;
     }
 })
-input.onButtonPressed(Button.AB, function () {
-    tie += 1
-    total += 1
+input.onButtonPressed(Button.AB, function () { //  if both player tie
+    tie += 1 //adds to tie
+    total += 1 //adds to total
     basic.showLeds(`
     # # # # #
     # # . # #
@@ -79,13 +79,13 @@ input.onButtonPressed(Button.AB, function () {
     `)
     basic.showNumber(tie)
 
-    if (tie == 5) {
+    if (tie == 5) { //check to see if both player tied.
         let percentC = (tie / total) * 100;
         let percentCString = ""
         percentCString = percentC.toString();
-        percentCString = percentCString.substr(0, percentCString.indexOf('.') + 2)
+        percentCString = percentCString.substr(0, percentCString.indexOf('.') + 2)//rounds to the tenth place
         basic.showString(" It's a tie." + percentCString + "% of games were tied")
-        basic.clearScreen()
+        basic.clearScreen() 
         playerOne = 0;
         playerTwo = 0;
         tie = 0;

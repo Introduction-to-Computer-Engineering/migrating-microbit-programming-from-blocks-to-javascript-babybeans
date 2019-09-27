e3d85694247bba43f5c08a46dcb2732d74344395
Jessica Gardner <48143153+babybeans@users.noreply.github.com>
2019-09-27 16:48:10 -0600

    Update and rename ledbrightnessdemo.js to activityseven-ledbrightnessdemo.js

--- activityseven-ledbrightnessdemo.js
+++ activityseven-ledbrightnessdemo.js
@@ -2,7 +2,7 @@ input.onButtonPressed(Button.A, function () {
     for (let i = 0; i <= 4; i++) {
         for (let j = 0; j <= 4; j++) {
             for (let y = 0; y <= 255; y += 10.2) {
-                led.plotBrightness(j, i, y);
+                led.plotBrightness(j, i, y); //plots leds from left to right, top to bottom, and dim to bright
                 basic.pause(50)
             }
         }
@@ -13,7 +13,7 @@ input.onButtonPressed(Button.B, function () {
     for (let i = 4; i >= 0; i--) {
         for (let j = 4; j >= 0; j--) {
             for (let y = 255; y >= 0; y -= 10.2) {
-                led.plotBrightness(j, i, y)
+                led.plotBrightness(j, i, y)// plots leds from right to left, bottom to top, then bright to dim
                 basic.pause(50)
             }
         }
@@ -21,7 +21,7 @@ input.onButtonPressed(Button.B, function () {
 })
 
 
-input.onButtonPressed(Button.AB, function () {
+input.onButtonPressed(Button.AB, function () { //displays checkerboard pattern
     basic.clearScreen()
     basic.showIcon(IconNames.Chessboard)
     led.setBrightness(led.brightness() - 51);
@@ -29,7 +29,7 @@ input.onButtonPressed(Button.AB, function () {
 })
 
 
-input.onGesture(Gesture.Shake, function () {
+input.onGesture(Gesture.Shake, function () { //displays a fading "A"
     basic.clearScreen();
     basic.showLeds(`
     . # # # . 
@@ -42,5 +42,5 @@ input.onGesture(Gesture.Shake, function () {
         led.setBrightness(led.brightness() - 51)
         basic.pause(100);
     }
-    control.reset();
+    control.reset();//resets program
 })
